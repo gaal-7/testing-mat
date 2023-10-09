181a9ff7c406bf12ea9aba9bf633dc0c65b6f9d4
gaal-7 <paco_garcia_alonso@hotmail.com>
2023-10-09 10:33:40 -0600

    cambios

--- src/app/factorial/factorial.ts
+++ src/app/factorial/factorial.ts
@@ -1,14 +1,13 @@
 export function factorial(x: number): number {
-    if (x < 0) {
-      return 0;
-    } else if (x === 0 || x === 1) {
-      return 1;
-    } else {
-      let result = 1;
-      for (let i = 2; i <= x; i++) {
-        result *= i;
-      }
-      return result;
+  if (x < 0 || x > 15) {
+    return 0;
+  } else if (x === 0 || x === 1) {
+    return 1;
+  } else {
+    let factorial = 1;
+    for (let i = 2; i <= x; i++) {
+      factorial *= i;
     }
+    return factorial;
   }
-  
+}
