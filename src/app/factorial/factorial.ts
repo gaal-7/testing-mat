e8af4f8bc7a80f5eaab70665a83eba1eeca88d09
gaal-7 <paco_garcia_alonso@hotmail.com>
2023-10-09 02:46:57 -0600

    add factorial branch #2

---
+++ src/app/factorial/factorial.ts
@@ -0,0 +1,14 @@
+export function factorial(x: number): number {
+    if (x < 0) {
+      return 0;
+    } else if (x === 0 || x === 1) {
+      return 1;
+    } else {
+      let result = 1;
+      for (let i = 2; i <= x; i++) {
+        result *= i;
+      }
+      return result;
+    }
+  }
+  
