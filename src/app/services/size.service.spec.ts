efa0e3ac8f0364c8f5c18b9885dd1ce602fe1666
omarotierra <2omarhernandez2@gmail.com>
2023-10-13 05:06:23 -0600

    finish media #4

--- src/app/services/size.service.spec.ts
+++ src/app/services/size.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import HttpClientTestingModule
 import { SizeService } from './size.service';
 
 describe('SizeService', () => {
   let service: SizeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Provide HttpClientTestingModule
+    });
     service = TestBed.inject(SizeService);
   });
 
